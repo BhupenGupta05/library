2e00470ae3431cc7089acce9bfa997d1804c2adc
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2024-01-23 19:19:24 +0530

    Books by genre

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks (author: String): [Book!]!
+    allBooks (author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,11 +112,15 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+        let filteredBooks = books
+
         if(args.author) {
-            return books.filter(book => book.author === args.author)
-        } else {
-            return books
+            filteredBooks = filteredBooks.filter(book => book.author === args.author)
         }
+        if(args.genre) {
+            filteredBooks = filteredBooks.filter(book => book.genres.includes(args.genre))
+        }
+        return filteredBooks
     },
     allAuthors: () => {
         return authors.map(author => ({
