f397a6247dcc2d9717b69f4119dd5fe45a9d8eb3
bhupengupta05@gmail.com <bhupengupta05@gmail.com>
2024-01-23 19:05:15 +0530

    Books of an author

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks (author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+        if(args.author) {
+            return books.filter(book => book.author === args.author)
+        } else {
+            return books
+        }
+    },
     allAuthors: () => {
         return authors.map(author => ({
             ...author, bookCount: books.filter(book => book.author === author.name).length
